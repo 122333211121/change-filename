b1ab2424c3db850c4c2b0eb0f3adb7683d268d0d
122333211121 <75063381+122333211121@users.noreply.github.com>
2021-07-19 10:23:52 +0800

    Add files via upload

---
+++ 文件改名.py
@@ -0,0 +1,23 @@
+import os
+import exifread
+ 
+def getExif(filename):
+    FIELD = 'EXIF DateTimeOriginal'
+    filedir = open(filename, 'rb')
+    tags = exifread.process_file(filedir)
+    filedir.close()
+    if FIELD in tags:
+        i = 1
+        new_name = str(tags[FIELD]).split(' ')[0] + '_' + str(i) + os.path.splitext(filename)[1]
+        new_name = new_name.replace(':', ' ')
+        while os.path.exists(new_name):    
+            i += 1
+            new_name = str(tags[FIELD]).split(' ')[0] + '_' + str(i) + os.path.splitext(filename)[1]
+            new_name = new_name.replace(':', ' ')
+        os.rename(filename, new_name)
+    else:
+        print('No {} found'.format(FIELD))
+ 
+for filename in os.listdir('.'):
+    if os.path.isfile(filename):
+        getExif(filename)
